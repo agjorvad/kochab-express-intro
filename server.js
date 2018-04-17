c7c8298c651cc4a524016564eb14f239e2f9d658
Alex Gjorvad <alexgjorvad@gmail.com>
2018-04-17 11:52:22 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/hey', function (req, res){
+    res.send('Howdy');
+});
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`)
+});
